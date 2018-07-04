f5af28a6b3d9cf418047f0aba615a957ba3f55eb
Yuan Fu <casouri@gmail.com>
2018-07-04 16:24:02 +0800

    Add moon as banner

--- basic/homepage/config.el
+++ basic/homepage/config.el
@@ -23,36 +23,84 @@
 (defvar moon-log-news nil
   "Whether to show news on homepage.")
 
+(defun moon-draw ()
+  "draw a pixel."
+  (interactive)
+  (insert "██"))
+
+(defvar moon-moon-banner-1 "
+          ████████
+      ██████████████
+  ██████████         █
+  ████████
+████████
+████████
+████████
+████████
+████████
+  ████████            █
+  ██████████        ██
+      ██████████████
+          ████████
+")
+
+(defvar moon-moon-banner-2 "
+           ████
+      ████████████ 
+   ███████         █ 
+  ██████  
+ █████  
+██████  
+██████  
+██████ 
+ █████   
+  ██████            █
+   ███████        ██
+      ████████████
+          █████ 
+")
+
+
 ;;
 ;; Func
 ;;
 
 
-(defun moon/draw-homepage ()
+(defun moon/draw-homepage (banner short-banner)
   "Draw MOON EMACS or MOON on the middle of current buffer.
 
 MOON is used when buffer's width is less than 86."
   (interactive)
   (unless noninteractive
-    (let ((banner-list (if (>= (window-width) 86)
-                           (split-string moon-banner "\n")
-                         (split-string moon-short-banner "\n"))))
+    (moon/draw-star 10)
+    (moon/draw-moon banner short-banner))
+  (moon/log-news)
+  (goto-char (point-min)))
+
+;; deprecate
+(defun moon/draw-star (num)
+  "Draw NUM stars randomly"
+  (interactive))
+
+(defun moon/draw-moon (banner short-banner)
+  "Draw moon"
+  (interactive)
+  (let* ((banner (if (>= (window-width) 86)
+                       banner
+                     short-banner))
+           (banner-list (split-string (car banner) "\n"))
+           (banner-width (or (nth 1 banner) (length (nth 1 banner-list))))
+           (pad-length (let ((total-extra (- (window-width) banner-width)))
+                         (if (wholenump total-extra)
+                             (/ total-extra 2)
+                           0))))
       (let ((space-to-insert
-	     (make-string
-	      (/ (let ((free-length
-                        (- (window-width)
-                           ;; first line could be empty, use second line for length
-                           (length (nth 1 banner-list)))))
-                   (if (wholenump free-length)
-                       free-length
-                     0)) 2) ?\s)))
+	     (make-string pad-length ?\s)))
         (insert (make-string 10 ?\n))
         (dolist (line banner-list)
 	  (insert space-to-insert)
 	  (insert line)
 	  (insert "\n")))))
-  (moon/log-news)
-  (goto-char (point-min)))
 
 (defun moon/log-news ()
   "Log core changes since last pull."
@@ -66,7 +114,7 @@ MOON is used when buffer's width is less than 86."
 ;;
 
 ;; Homepage
-(add-hook 'moon-init-hook #'moon/draw-homepage t)
+(add-hook 'moon-init-hook (lambda () (moon/draw-homepage `(,moon-moon-banner-1 40) '(moon-moon-banner))) t)
 (add-hook 'moon-post-init-hook #'moon-display-benchmark t)
 
 
