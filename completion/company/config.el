5c213bc8c8df6fb6ac3505d119ec34a9805e1367
Yuan Fu <casouri@gmail.com>
2018-07-05 12:57:26 +0800

    Add company-box

--- completion/company/config.el
+++ completion/company/config.el
@@ -4,7 +4,8 @@
   :config
   (setq company-idle-delay 0
         company-minimum-prefix-length 2
-        company-dabbrev-downcase nil)
+        company-dabbrev-downcase nil
+        company-tooltip-limit 20)
   (define-key company-active-map (kbd "M-n") nil)
   (define-key company-active-map (kbd "M-p") nil)
   (define-key company-active-map (kbd "C-n") #'company-select-next)
@@ -13,12 +14,19 @@
   (define-key company-active-map [(shift return)] #'moon/cancel-completion-and-next-line)
   )
 
-(load| short-finger)
-
-(add-hook 'org-mode-hook #'short-finger-mode)
-(add-hook 'markdown-mode-hook #'short-finger-mode)
-
-;; (post-config| general
-;;   (general-define-key :states '(normal insert emacs visual)
-;; 		      "TAB" #'company-indent-or-complete-common))
-
+(use-package| company-box
+  ;; TODO common face
+  :init
+  ;; (defface company-box-candidates
+  ;;   '((t (:inherit company-tooltip)))
+  ;;   "Override face of company-box.")
+  ;; (defface company-box-selection
+  ;;   '((t (:inherit company-tooltip-selection)))
+  ;;   "Override face of company-box.")
+  ;; (defface company-box-annotation
+  ;;   '((t (:inherit company-tooltip-annotation)))
+  ;;   "Override face of company-box.")
+  :config
+  (setq company-box-enable-icon nil)
+  (setq company-box-doc-delay 0.3)
+  :hook (company-mode . company-box-mode))
