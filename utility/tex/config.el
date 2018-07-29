5358c310634971116ec016cbcb346655569bb754
Yuan Fu <casouri@gmail.com>
2018-07-29 20:38:28 +0800

    Fix tex config

--- utility/tex/config.el
+++ utility/tex/config.el
@@ -1,14 +1,11 @@
-(use-package| tex-site
-  :defer
-  :config
-  ;; suggested by auctex manual
-  (setq TeX-auto-save t)
-  (setq TeX-parse-self t)
-  (setq TeX-syntactic-comment t)
-  (setq-default TeX-master nil)
+;; suggested by auctex manual
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq TeX-syntactic-comment t)
+(setq-default TeX-master nil)
 
-  (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
-  (add-hook 'LaTeX-mode-hook 'TeX-PDF-mode))
+(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+(add-hook 'LaTeX-mode-hook 'TeX-PDF-mode)
 
 (use-package| latex-preview-pane
   :commands latex-preview-pane-mode)
