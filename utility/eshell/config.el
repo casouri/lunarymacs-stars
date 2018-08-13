a695724041ffb3c67923a72f301043bb7dc07e1f
Yuan Fu <casouri@gmail.com>
2018-08-13 17:07:13 +0800

    Extend eshell functionality

--- utility/eshell/config.el
+++ utility/eshell/config.el
@@ -1,6 +1,7 @@
 (post-config| general
   (moon-default-leader
-    "C-o s" (lambda () "Open in eshell." (interactive) (eshell default-directory)))
+    "C-o s" (lambda () "Open in eshell." (interactive) (eshell default-directory))
+    "us" #'counsel-switch-to-eshell-buffer)
   (general-define-key
    :prefix "C-c"
    :keymap 'eshell-mode-map
@@ -23,6 +24,29 @@
 ;;; Config
 
 (setq eshell-directory-name (concat moon-star-dir "utility/eshell/"))
+(setq eshell-last-dir-ring-size 500)
+(setq eshell-cp-interactive-query t
+       eshell-ln-interactive-query t
+       eshell-mv-interactive-query t
+       eshell-rm-interactive-query t
+       eshell-mv-overwrite-files nil)
+
+(defvar default-system-name (system-name)
+  "The system name of this computer.")
+
+(setq eshell-prompt-regexp "^[^!λ\n]*[!λ] "
+      eshell-prompt-function
+      (lambda ()
+        (concat
+	 (unless (string= (system-name) default-system-name)
+           (concat (user-login-name) "@ " (system-name)))
+	 (propertize (abbreviate-file-name (eshell/pwd)) 'face '(:foreground "#98C379"))
+	 (if (= (user-uid) 0)
+             (propertize " ! " 'face '(:forground "#FA5754"))
+           (propertize " λ " 'face '(:foreground "#51afef"))))))
+
+
+;;;; Sync buffer name and pwd
 
 (defun eshell-sync-dir-buffer-name ()
   "Change eshell buffer name by directory change."
@@ -34,7 +58,52 @@
                    t)))
 
 (add-hook 'eshell-directory-change-hook #'eshell-sync-dir-buffer-name)
+(add-hook 'eshell-mode-hook #'eshell-sync-dir-buffer-name)
 
-(post-config| general
-  (moon-default-leader
-   "us" #'eshell))
+
+;;;; Validate command
+
+(defun moon-validate-command ()
+  "Validate eshell command."
+  (save-excursion
+    (beginning-of-line)
+    (re-search-forward (format "%s\\([^ ]*\\)" eshell-prompt-regexp)
+                       (line-end-position)
+                       t)
+    (let ((beg (match-beginning 1))
+          (end (match-end 1))
+          (command (match-string 1)))
+      (put-text-property beg end
+       'face `(:foreground ,(if (executable-find command)
+                                "#98C379"
+                              mac-red))))))
+
+(add-hook 'eshell-mode-hook (lambda ()
+                              "Add `moon-validate-command' to `post-command-hook'."
+                              (add-hook 'post-command-hook #'moon-validate-command t t)))
+
+;;;; Swtich eshell
+
+(after-load| counsel
+  (defun counsel-switch-to-eshell-buffer ()
+    "Switch to a shell buffer, or create one."
+    (interactive)
+    (ivy-read "Shell buffer: " (counsel--buffers-with-mode #'eshell-mode)
+              :action #'counsel--switch-to-shell
+              :caller 'counsel-switch-to-shell-buffer)))
+
+;; I have pwd buffer name, no need for this
+
+;; (defvar ivy-filthy-rich-switch-eshell-format
+;;   '(((value . (lambda (_) _)) (prop . 0.4))
+;;     ((value . (lambda (cand) (list (buffer-local-value 'default-directory (get-buffer cand)))))
+;;      (prop . 0.6)
+;;      (face . (:foreground "#98C379")))))
+
+(defvar ivy-filthy-rich-switch-eshell-format
+  `(((value . (lambda (_) _)) (prop . 0.6) (face . (:foreground ,lunary-pink)))))
+
+
+(after-load| ivy-filthy-rich
+  (add-to-list 'ivy-filthy-rich-transformer-alist
+               (ivy-filthy-rich-make-transformer 'counsel-switch-to-eshell-buffer 'ivy-filthy-rich-switch-eshell-format)))
