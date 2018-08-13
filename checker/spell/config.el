46904712fa1e44d932164e7c6506d39715d1b68b
Yuan Fu <casouri@gmail.com>
2018-08-13 17:07:32 +0800

    Add some grammar checking tool

--- checker/spell/config.el
+++ checker/spell/config.el
@@ -2,10 +2,15 @@
 
 (use-package| flyspell
   :commands moon/toggle-spell-check
+  :hook ((fundamenta-mode . flyspell-mode)
+         (prog-mode . flyspell-prog-mode))
   :config
-  (when (executable-find "hunspell")
-    (setq-default ispell-program-name "hunspell")
-    (setq ispell-really-hunspell t)))
+  (setq flyspell-issue-message-flag nil)
+  ;; (when (executable-find "hunspell")
+  ;;   (setq-default ispell-program-name "hunspell")
+  ;;   (setq ispell-really-hunspell t)
+  ;;   (setq ispell-dictionary "en_US"))
+  )
 
 
 (post-config| general
@@ -14,11 +19,26 @@
 
 
 (use-package| flyspell-correct-ivy
-  :after flyspell
+  :after flyspell ivy
   :config
   ;; https://www.emacswiki.org/emacs/FlySpell
   (setq flyspell-issue-message-flag nil))
 
 (post-config| general
   (moon-default-leader
-    "ef" #'flyspell-correct-previous))
+    "ef" #'flyspell-correct-previous
+    "eg" #'langtool-check
+    "ec" #'langtool-correct-buffer
+    "is" #'synonyms))
+
+(use-package| langtool
+  :commands langtool-check
+  :config
+  (setq langtool-language-tool-server-jar "/usr/local/Cellar/languagetool/4.1/libexec/languagetool-server.jar")
+  (setq langtool-java-bin "/usr/bin/java"))
+
+(use-package| (writegood-mode :fetcher github :repo "bnbeckwith/writegood-mode")
+  :hook ((fundamental-mode org-mode) . writegood-mode))
+
+(use-package| (synonyms :fetcher url :url "https://www.emacswiki.org/emacs/download/synonyms.el")
+  :commands synonyms)
