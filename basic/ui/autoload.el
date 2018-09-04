97cf275715766f7527b21a2a3f1f0c442894c3ca
Yuan Fu <casouri@gmail.com>
2018-09-04 17:44:07 -0400

    Put back moon/split-to

--- basic/ui/autoload.el
+++ basic/ui/autoload.el
@@ -6,6 +6,20 @@
   (interactive)
   (desktop-read moon-local-dir))
 
+(defun moon/split-to (direction)
+  "Split window and switch to it according to DIRECTION.
+Accept one character for DIRECTION.
+  k     ^
+h   l <   >
+  j     v
+"
+  (interactive "cswitch to window: h/j/k/l")
+  (pcase direction
+    (?k (split-window-below))                 ; up
+    (?j (select-window (split-window-below))) ; down
+    (?h (split-window-right))                 ; left
+    (?l (select-window (split-window-right))) ; right
+   ))
 
 ;;
 ;; Moody
