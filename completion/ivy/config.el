751fb7126b05db71f834356be0da0d434418ec48
Yuan Fu <casouri@gmail.com>
2018-09-19 19:45:55 -0400

    Make M-x use counsel and add C-SPC to M-x binding

--- completion/ivy/config.el
+++ completion/ivy/config.el
@@ -23,37 +23,40 @@
        (general-define-key
         :keymaps 'override
         "<C-return>" #'ivy-switch-buffer))
+  (general-define-key
+   "M-x" #'counsel-M-x)
   (moon-default-leader
     ;; other
-    "SPC" #'counsel-M-x
+    "C-SPC" #'counsel-M-x
+    "SPC"   #'counsel-M-x
     ;; files
-    "ff"  #'counsel-find-file
-    "fr"  #'counsel-recentf
-    "fL"  #'counsel-locate
+    "ff"    #'counsel-find-file
+    "fr"    #'counsel-recentf
+    "fL"    #'counsel-locate
     ;; help
-    "hb"  #'counsel-descbinds
-    "hf"  #'counsel-describe-function
-    "hF"  #'counsel-describe-face
-    "hl"  #'counsel-find-library
-    "hm"  #'spacemacs/describe-mode
-    "hk"  #'describe-key
-    "ho"  #'describe-symbol
-    "hv"  #'counsel-describe-variable
-    "hd"  #'apropos
-    "hi"  #'counsel-info-lookup-symbol
-    "hR"  #'spacemacs/counsel-search-docs
-    "?"   #'counsel-apropos
+    "hb"    #'counsel-descbinds
+    "hf"    #'counsel-describe-function
+    "hF"    #'counsel-describe-face
+    "hl"    #'counsel-find-library
+    "hm"    #'spacemacs/describe-mode
+    "hk"    #'describe-key
+    "ho"    #'describe-symbol
+    "hv"    #'counsel-describe-variable
+    "hd"    #'apropos
+    "hi"    #'counsel-info-lookup-symbol
+    "hR"    #'spacemacs/counsel-search-docs
+    "?"     #'counsel-apropos
     ;; insert
-    "iu"  #'counsel-unicode-char
+    "iu"    #'counsel-unicode-char
     ;; search
-    "si"  #'counsel-imenu
-    "ss"  #'moon/smart-swiper
-    "sr"  #'counsel-rg
+    "si"    #'counsel-imenu
+    "ss"    #'moon/smart-swiper
+    "sr"    #'counsel-rg
     ;; themes
-    "Ts"  #'counsel-load-theme
+    "Ts"    #'counsel-load-theme
     ;; buffer
-    "bb"  #'ivy-switch-buffer
-    "RET" #'counsel-recentf
+    "bb"    #'ivy-switch-buffer
+    "RET"   #'counsel-recentf
     )
   (general-define-key
    :keymaps 'override
@@ -84,9 +87,6 @@
                         counsel-yank-pop
                         counsel-descbinds counsel-org-capture
                         counsel-grep-or-swiper))
-(use-package| smex
-  :commands (smex smex-major-mode-commands)
-  :config (setq smex-save-file (concat moon-local-dir "smex-items")))
 
 (use-package| (ivy-filthy-rich :repo "casouri/ivy-filthy-rich" :fetcher github)
   :after counsel
