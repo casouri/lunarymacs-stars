1c6dde55a59abb1fd1c32353e8676449b564fa19
Yuan Fu <casouri@gmail.com>
2018-09-19 00:15:16 -0400

    Put all starup code into one place

--- lang/lsp/config.el
+++ lang/lsp/config.el
@@ -1,29 +1,40 @@
-;; -*- lexical-binding: t -*-
+;;; config.el --- Language server protocol      -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;; 
+
+;;; Code:
+;;
+
 
 (defvar moon-smart-toggle-lsp-ui t
-  "Whether to toggle lsp-ui doc and sideline automatically
-depending on window width.")
+  "Whether to toggle lsp-ui doc and sideline automatically depending on window width.")
 
 (defvar moon-smart-toggle-threshold 120
-  "If window width is above threshold, keep lsp-ui-doc/sideline on,
-if under, turn them off.")
+  "If window width is above threshold, keep lsp-ui-doc/sideline on, if under, turn them off.")
 
 (use-package| lsp-mode
-  :defer t
-  :init
-  (defun setup-lsp ()
-    (require 'lsp-imenu)
-    (add-hook 'lsp-after-open-hook #'lsp-enable-imenu)
-    (setq lsp-enable-eldoc nil)
-    (setq lsp-message-project-root-warning t)
-    (setq create-lockfiles nil)))
+  :defer t)
 
-(with-eval-after-load 'lsp-ui-mode
-  (require 'lsp-flycheck))
+(with-eval-after-load 'lsp-mode
+  (require 'lsp-imenu)
+  (add-hook 'lsp-after-open-hook #'lsp-enable-imenu)
+  (setq lsp-enable-eldoc nil)
+  (add-hook 'lsp-after-ope-hook (lambda () (eldoc-mode -1)))
+  (setq lsp-message-project-root-warning t)
+  (setq create-lckfiles nil)
+  (dolist (face '(lsp-face-highlight-read
+                  lsp-face-highlight-textual
+                  lsp-face-highlight-write))
+    (set-face-attribute face nil
+                        :foreground nil
+                        :distant-foreground nil
+                        :background nil
+                        :inherit 'lazy-highlight))
 
-(use-package| lsp-ui
-  :after lsp-mode
-  :config
+  (require 'lsp-ui)
+  (with-eval-after-load 'fly-check
+    (require 'lsp-flycheck))
   (add-hook 'lsp-mode-hook #'lsp-ui-mode)
   ;; completion
   (setq lsp-enable-completion-at-point t)
@@ -44,14 +55,43 @@ if under, turn them off.")
   ;; peek color
   (moon/sync-peek-face)
   (add-hook 'moon-load-theme-hook #'moon/sync-peek-face)
+
+  (with-eval-after-load 'company
+    (require 'company-lsp)
+    (setq company-lsp-async t)
+    (add-to-list 'company-backends 'company-lsp))
   )
 
-(use-package| company-lsp
-  :after (company lsp-mode)
-  :init  
-  (setq company-lsp-async t)
-  (add-to-list 'company-backends 'company-lsp))
+;; (use-package| lsp-ui
+;;   :after lsp-mode
+;;   :config
+;;   (require 'lsp-flycheck)
+;;   (add-hook 'lsp-mode-hook #'lsp-ui-mode)
+;;   ;; completion
+;;   (setq lsp-enable-completion-at-point t)
+;;   ;; ui
+;;   (setq lsp-ui-sideline-enable t)
+;;   (setq lsp-ui-doc-enable t)
+;;   (setq lsp-ui-doc-position 'top)
+;;   (setq lsp-ui-doc-header t)
+;;   (setq lsp-ui-doc-include-signature t)
+;;   ;; smart toggle lsp-ui doc & sideline
+;;   (add-hook 'window-configuration-change-hook
+;;             #'moon/smart-toggle-lsp-ui)
+;;   (add-hook 'lsp-ui-mode-hook
+;;             #'moon/smart-toggle-lsp-ui)
+;;   ;; if manually toggled doc or sideline, disable smart-toggle
+;;   (advice-add 'lsp-ui-doc-mode :after #'moon-force-lsp-ui)
+;;   (advice-add 'lsp-ui-sideline-mode :after #'moon-force-lsp-ui)
+;;   ;; peek color
+;;   (moon/sync-peek-face)
+;;   (add-hook 'moon-load-theme-hook #'moon/sync-peek-face))
 
+;; (use-package| company-lsp
+;;   :after (company lsp-mode)
+;;   :init  
+;;   (setq company-lsp-async t)
+;;   (add-to-list 'company-backends 'company-lsp))
 
 (post-config| general
   (with-eval-after-load 'lsp-mode
@@ -62,3 +102,5 @@ if under, turn them off.")
         "lR" #'lsp-rename
         "lf" #'lsp-format-buffer
         ))))
+
+;;; config.el ends here
