2d14ba46b45076c93523e77ec6bfa5c04882bcda
Yuan Fu <casouri@gmail.com>
2018-09-19 03:53:55 -0400

    Update path config

--- os/mac/config.el
+++ os/mac/config.el
@@ -6,6 +6,16 @@
 ;;; Code:
 ;;
 
+(defun get-path ()
+  (let ((path-raw (shell-command-to-string "/usr/libexec/path_helper")))
+    (string-match "PATH=\"\\(.+?\\)\"" path-raw)
+    (match-string 1 path-raw)))
+
+(let ((path (get-path)))
+  (setenv "PATH" path)
+  (setq exec-path (append exec-path (split-string path ":"))))
+
+
 (load| path)
 
 ;;; config.el ends here
--- os/mac/path.el
+++ os/mac/path.el
@@ -1,38 +1,39 @@
-;;; ENV
+;;; PATH
 
 (defun add-path (path)
   "Add PATH to path."
   (setenv "PATH" (concat path ":" (getenv "PATH"))))
 
-;; python
-(add-path "/usr/local/lib/python3.6:/usr/local/lib/python3.6/site-packages:/System/Library/Frameworks/Python.framework/Versions/2.7:/Users/yuan/bin")
-
-;;syntax highlighting for cheat
-(setenv "CHEATCOLORS" "true")
+;; ;; python
+;; (add-path "/usr/local/lib/python3.6:/usr/local/lib/python3.6/site-packages:/System/Library/Frameworks/Python.framework/Versions/2.7:/Users/yuan/bin")
 
 ;; let make point to gmake
 ;; If you need to use these commands with their normal names, you
 ;; can add a "gnubin" directory to your PATH from your bashrc like:
 (add-path "/usr/local/opt/make/libexec/gnubin")
 
+;; ;; rust
+;; (add-path "$HOME/.cargo/bin")
+
+;; ;; tex support
+;; (add-path "/Library/TeX/texbin/")
+
+;; ;; node
+;; (add-path "$PATH:/usr/local/Cellar/node/10.10.0/")
+
+;;; ENV
 ;; Additionally, you can access their man pages with normal names if you add
 ;; the "gnuman" directory to your MANPATH from your bashrc as well:
 (setenv "MANPATH" "/usr/local/opt/make/libexec/gnuman:$MANPATH")
 
-;; rust
-(add-path "$HOME/.cargo/bin")
-
 ;; pipenv shell now create .venv under project dir
 (setenv "PIPENV_VENV_IN_PROJECT" "true")
-
-;; tex support
-(add-path "/Library/TeX/texbin/")
 
 ;; just print everthing and use Emacs as pager
 (setenv "PAGER" "cat")
 
-;; node
-(add-path "$PATH:/usr/local/Cellar/node/10.10.0/")
+;;syntax highlighting for cheat
+(setenv "CHEATCOLORS" "true")
 
 ;;; RC
 
