d5cf4358c8f99b74d7407770ab43a73ba15dfd96
Yuan Fu <casouri@gmail.com>
2018-09-19 17:26:17 -0400

    Disable group switching binding

--- basic/ui/config.el
+++ basic/ui/config.el
@@ -14,8 +14,9 @@
   (general-define-key
    "s-b" #'tabbar-backward
    "s-f" #'tabbar-forward
-   "s-n" #'tabbar-forward-group
-   "s-p" #'tabbar-backward-group)
+   ;; "s-n" #'tabbar-forward-group
+   ;; "s-p" #'tabbar-backward-group
+   )
   (moon-default-leader
     "tb" #'awesome-tab-mode))
 
