16b12ca87a24abb9240be223b829a6a09af3a367
Yuan Fu <casouri@gmail.com>
2018-09-22 13:34:22 -0400

    Remove neotree

--- utility/dir/config.el
+++ utility/dir/config.el
@@ -1,19 +1,17 @@
 ;; -*- lexical-binding: t -*-
 
-;;
-;; Func
-;;
+;;; Key
 
-
-(use-package| neotree
-  :commands neotree-toggle
-  :init
-  (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
-  (setq neo-smart-open t)
-  (setq projectile-switch-project-action 'neotree-projectile-action)
-  (setq neo-show-hidden-files t)
-  :config
-  (require 'all-the-icons))
+(post-config| general
+  (general-define-key
+   :keymaps 'dired-mode-map
+   "C-c C-f" #'dired-narrow-fuzzy
+   "b" #'dired-up-directory
+   "q" #'quit-window
+   "j" #'next-line
+   "k" #'previous-line
+   "h" #'dired-up-directory
+   "l" #'dired-find-file))
 
 (post-config| general
   (moon-default-leader
@@ -28,6 +26,19 @@
    "q" #'neotree-hide
    "RET" #'neotree-enter))
 
+;;; Package
+
+(use-package| neotree
+  :commands neotree-toggle
+  :init
+  (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+  (setq neo-smart-open t)
+  (setq projectile-switch-project-action 'neotree-projectile-action)
+  (setq neo-show-hidden-files t)
+  :config
+  (require 'all-the-icons))
+
+
 (use-package| ranger
   :init (setq ranger-show-hidden t)
   :commands ranger)
@@ -35,13 +46,6 @@
 (use-package| dired-narrow
   :commands dired-narrow)
 
-(post-config| general
-  (general-define-key
-   :keymaps 'dired-mode-map
-   "C-c C-f" #'dired-narrow-fuzzy
-   "b" #'dired-up-directory
-   "q" #'quit-window
-   "j" #'next-line
-   "k" #'previous-line
-   "h" #'dired-up-directory
-   "l" #'dired-find-file))
+;;; Config
+
+(add-hook 'dired-mode-hook #'auto-revert-mode)
