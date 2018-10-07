57e6fdd42f085c334c822c985777ea64874ff160
Yuan Fu <casouri@gmail.com>
2018-10-07 03:36:49 -0400

    Update Org

--- utility/org/config.el
+++ utility/org/config.el
@@ -24,6 +24,19 @@
 ;;             (setq buffer-face-mode-face '(:family "Courier New" :height 150))
 ;;             (buffer-face-mode)))
 
+;;; Keys
+
+(post-config| general
+  (moon-default-leader
+    "ot" '((lambda () (interactive)
+             (find-file moon-todo-file)) :which-key "jump todo")
+    "oc" #'org-capture
+    "oa" #'org-agenda))
+
+(defvar moon-todo-file "~/note/todo.org")
+
+;;; Packages
+
 (use-package| toc-org
   :commands (toc-org-enable
              toc-org-insert-toc))
@@ -39,36 +52,50 @@
   (add-hook 'org-mode-hook #'olivetti-mode)
   :commands olivetti-mode)
 
+;;; Function
+
+(defun moon/new-blog (title)
+  "Make a new blog post with TITLE."
+  (interactive "M")
+  (let* ((year (shell-command-to-string "echo -n $(date +%Y)"))
+         (dir-file-name (downcase (replace-regexp-in-string " " "-" title)))
+         (dir-path (concat (format  "~/p/casouri/note/%s/"
+                                    year)
+                           dir-file-name))
+         (file-path (concat dir-path
+                            "/index.org")))
+    (mkdir dir-path)
+    (find-file file-path)
+    (insert (format "#+OPTIONS: html-style:nil
+#+HTML_HEAD: <link rel=\"stylesheet\" type=\"text/css\" href=\"/note/style.css\"/>
+#+HTML_HEAD_EXTRA: <script type=\"text/javascript\" src=\"/note/script.js\"></script>
+#+HTML_LINK_UP: /note
+#+HTML_LINK_HOME: /note
+#+TITLE: %s
+#+DATE:
+"
+                    title))
+    (kill-new (format "- [[./%s/%s/][%s]]"
+                      year
+                      dir-file-name
+                      title))
+    (save-buffer)
+    (find-file "~/p/casouri/note/index.org")))
 
 ;;; Config
 
-;;;; org-capture
+;;;; Org Agenda
 
-(post-config| general
-  (moon-default-leader
-    "uo" '(:ignore t :which-key "org")
-    "uoc" #'org-capture
-    ))
+(setq org-agenda-files (list moon-todo-file))
 
-
+;;;; Org Capture
 
 (with-eval-after-load 'org-capture
   (setq org-default-notes-file "~/note/index.org")
   (setq org-capture-templates
         (append org-capture-templates
-                `(("l" "Links")
-                  ("la" "Design related links." item (file+olp "~/note/index.org" "Links" "Design links") "- %? :: ")
-                  ("lp" "programming related links." item (file+olp "~/note/index.org" "Links" "Programming links") "- %? :: ")
-                  ("lw" "Web related links." item (file+olp "~/note/index.org" "Links" "Web links") "- %? :: ")
-                  ("lb" "Blog links." item (file+olp "~/note/index.org" "Links" "Blog links") "- %? :: ")
-                  ("lu" "Utility links." item (file+olp "~/note/index.org" "Links" "Utility links") "- %? :: ")
-                  ("lt" "Tools" item (file+olp "~/note/index.org" "Links" "Tool links") "- %? :: ")
-                  ("n" "Notes")
-                  ("nr" "Random notes" entry (file+olp "~/note/index.org" "Notes" "Random notes") "*** %?")
-                  ("nv" "Vim notes" entry (file+olp "~/note/index.org" "Notes" "Vim notes") "*** %? ")
-                  ("ne" "Emacs notes" entry (file+olp "~/note/index.org" "Notes" "Emacs notes") "*** %?")
-                  ("nt" "Tutorial notes" entry (file+olp "~/note/index.org" "Notes" "Tutorial notes") "*** %?")
-                  ("t" "Tasks")
-                  ("ti" "Ideas" entry (file+olp "~/note/index.org" "Tasks" "Ideas") "*** TODO %?")
-                  ("tt" "Tasks" entry (file+olp "~/note/index.org" "Tasks" "Tasks") "*** TODO %?")
+                `(("t" "TODOs")
+                  ("te" "Emacs" entry (file+olp "~/note/todo.org" "Emacs") "** TODO %?")
+                  ("to" "Other" entry (file+olp "~/note/todo.org" "Other") "** TODO %?")
+                  ("ts" "School" entry (file+olp "~/note/todo.org" "School") "** TODO %?")
                   ))))
