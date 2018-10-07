974d7501506eacf84418b6b40fb7ddf2358fd51c
Yuan Fu <casouri@gmail.com>
2018-10-07 03:38:55 -0400

    Put to bak

---
+++ utility/org/config.old.el
@@ -0,0 +1,20 @@
+(with-eval-after-load 'org-capture
+  (setq org-default-notes-file "~/note/index.org")
+  (setq org-capture-templates
+        (append org-capture-templates
+                `(("l" "Links")
+                  ("la" "Design related links." item (file+olp "~/note/index.org" "Links" "Design links") "- %? :: ")
+                  ("lp" "programming related links." item (file+olp "~/note/index.org" "Links" "Programming links") "- %? :: ")
+                  ("lw" "Web related links." item (file+olp "~/note/index.org" "Links" "Web links") "- %? :: ")
+                  ("lb" "Blog links." item (file+olp "~/note/index.org" "Links" "Blog links") "- %? :: ")
+                  ("lu" "Utility links." item (file+olp "~/note/index.org" "Links" "Utility links") "- %? :: ")
+                  ("lt" "Tools" item (file+olp "~/note/index.org" "Links" "Tool links") "- %? :: ")
+                  ("n" "Notes")
+                  ("nr" "Random notes" entry (file+olp "~/note/index.org" "Notes" "Random notes") "*** %?")
+                  ("nv" "Vim notes" entry (file+olp "~/note/index.org" "Notes" "Vim notes") "*** %? ")
+                  ("ne" "Emacs notes" entry (file+olp "~/note/index.org" "Notes" "Emacs notes") "*** %?")
+                  ("nt" "Tutorial notes" entry (file+olp "~/note/index.org" "Notes" "Tutorial notes") "*** %?")
+                  ("t" "Tasks")
+                  ("ti" "Ideas" entry (file+olp "~/note/index.org" "Tasks" "Ideas") "*** TODO %?")
+                  ("tt" "Tasks" entry (file+olp "~/note/index.org" "Tasks" "Tasks") "*** TODO %?")
+                  ))))
