bf7aa15c9ccc5e3cb57cf007bf30c4e8128fc11f
Yuan Fu <casouri@gmail.com>
2018-12-05 15:12:27 -0500

    Add a workaround for tab width in javescript mode

--- lang/javascript/config.el
+++ lang/javascript/config.el
@@ -15,7 +15,9 @@
       (add-hook 'js-mode-hook #'eglot-ensure)
       (add-hook 'typescript-mode #'eglot-ensure)
       (push '(js-mode . eglot-format-buffer) moon-smart-format-alist)
-      (push '(typescript-mode . eglot-format-buffer) moon-smart-format-alist))
+      (push '(typescript-mode . eglot-format-buffer) moon-smart-format-alist)
+      ;; KLUDGE https://github.com/joaotavora/eglot/issues/157
+      (add-hook 'javascript-mode-hook (lambda () (setq-local tab-width 2))))
   (use-package| lsp-typescript
     :defer t)
 
@@ -44,7 +46,6 @@
 
   (push '(js-mode . lsp-format-buffer) moon-smart-format-alist)
   (push '(typescript-mode . lsp-format-buffer) moon-smart-format-alist))
-
 
 
 ;;; indent
