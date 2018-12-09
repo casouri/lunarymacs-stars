f634171df28ff4d646b469cf1f6b42e4954b251c
Yuan Fu <casouri@gmail.com>
2018-12-09 01:47:15 -0500

    Disable neotree and ranger until I need them

--- utility/dir/config.el
+++ utility/dir/config.el
@@ -16,38 +16,38 @@
     "l" #'dired-find-file)
    nil))
 
-(post-config| general
-  (moon-default-leader
-    "tn" #'neotree-toggle
-    "tr" #'ranger
-    "th" #'moon/toggle-hidden-file)
-  (general-define-key
-   :states 'normal
-   :keymaps 'neotree-mode-map
-   "TAB" #'neotree-enter
-   "SPC" #'neotree-quick-look
-   "q" #'neotree-hide
-   "RET" #'neotree-enter))
+;; (post-config| general
+;;   (moon-default-leader
+;;     "tn" #'neotree-toggle
+;;     "tr" #'ranger
+;;     "th" #'moon/toggle-hidden-file)
+;;   (general-define-key
+;;    :states 'normal
+;;    :keymaps 'neotree-mode-map
+;;    "TAB" #'neotree-enter
+;;    "SPC" #'neotree-quick-look
+;;    "q" #'neotree-hide
+;;    "RET" #'neotree-enter))
 
 ;;; Package
 
-(use-package| neotree
-  :commands neotree-toggle
-  :init
-  (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
-  (setq neo-smart-open t)
-  (setq projectile-switch-project-action 'neotree-projectile-action)
-  (setq neo-show-hidden-files t)
-  :config
-  (require 'all-the-icons))
+;; (use-package| neotree
+;;   :commands neotree-toggle
+;;   :init
+;;   (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+;;   (setq neo-smart-open t)
+;;   (setq projectile-switch-project-action 'neotree-projectile-action)
+;;   (setq neo-show-hidden-files t)
+;;   :config
+;;   (require 'all-the-icons))
 
 
-(use-package| ranger
-  :init (setq ranger-show-hidden t
-              ;; otherwise ranger binds ranger-key (C-p)
-              ;; to `deer-from-dired' in dired mode
-              ranger-key nil)
-  :commands ranger)
+;; (use-package| ranger
+;;   :init (setq ranger-show-hidden t
+;;               ;; otherwise ranger binds ranger-key (C-p)
+;;               ;; to `deer-from-dired' in dired mode
+;;               ranger-key nil)
+;;   :commands ranger)
 
 (use-package| dired-narrow
   :commands dired-narrow)
