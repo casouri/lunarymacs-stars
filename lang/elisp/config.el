00a4b0ccf632695b7eea0029e85d4c3fe5acc00b
Yuan Fu <casouri@gmail.com>
2018-12-11 14:14:43 -0500

    Use flymake in emacs-lisp-mode
    And reformat config.el

--- lang/elisp/config.el
+++ lang/elisp/config.el
@@ -1,5 +1,23 @@
 ;; -*- lexical-binding: t -*-
 
+;;; Key
+(post-config| general
+  (moon-cx-leader
+    :keymaps 'emacs-lisp-mode-map
+    ;; eval
+    "w" '(:ignore t :which-key "eval")
+    "w e" #'eval-last-sexp
+    "w r" #'eval-region
+    "w f" #'eval-defun
+    "w b" #'eval-buffer))
+
+;;; Package
+(use-package| aggressive-indent
+  :defer t
+  :init
+  (add-hook 'emacs-lisp-mode-hook #'aggressive-indent-mode)
+  (add-hook 'css-mode-hook #'aggressive-indent-mode))
+
 ;; (use-package| lispyville
 ;;   :hook (emacs-lisp-mode . lispyville-mode)
 ;;   :config
@@ -11,18 +29,5 @@
 ;;      slurp/barf-cp)))
 
 
-(post-config| general
-  (moon-cx-leader
-    :keymaps 'emacs-lisp-mode-map
-    ;; eval
-    "w" '(:ignore t :which-key "eval")
-    "w e" #'eval-last-sexp
-    "w r" #'eval-region
-    "w f" #'eval-defun
-    "w b" #'eval-buffer))
-
-(use-package| aggressive-indent
-  :defer t
-  :init
-  (add-hook 'emacs-lisp-mode-hook #'aggressive-indent-mode)
-  (add-hook 'css-mode-hook #'aggressive-indent-mode))
+;;; Config
+(add-hook 'emacs-lisp-mode-hook #'flymake-mode)
