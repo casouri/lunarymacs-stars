ea38094b65ca1769bf885cf28b26cc223981f421
Yuan Fu <casouri@gmail.com>
2018-12-13 02:27:07 -0500

    Remove license from config

--- basic/edit/autoload.el
+++ basic/edit/autoload.el
@@ -67,28 +67,9 @@ buffer is not visiting a file."
          (feature (file-name-base (buffer-file-name))))
     (insert (format ";;; %s --- %s      -*- lexical-binding: t; -*-
 
-;; Copyright (C) %s  Yuan Fu
-
 ;; Author: Yuan Fu <casouri@gmail.com>
 
 ;;; This file is NOT part of GNU Emacs
-
-;;; License
-;;
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 3, or (at your option)
-;; any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program; see the file COPYING.  If not, write to
-;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
-;; Floor, Boston, MA 02110-1301, USA.
 
 ;;; Commentary:
 ;; 
