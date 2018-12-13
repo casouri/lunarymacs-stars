5bf8c0f3292b07724a1610cdd4275dd01bc13561
Yuan Fu <casouri@gmail.com>
2018-12-13 15:55:50 -0500

    Remove functions in eshell star
    - moon/toggle-eshell: went into aweshell
    - moon/esh-history: no need
    - counsel-switch-to-eshell-buffer: use helm now

--- utility/eshell/config.el
+++ utility/eshell/config.el
@@ -13,25 +13,6 @@
   (general-define-key
    "s-e" #'aweshell-toggle))
 
-(defun moon/toggle-eshell ()
-  "Toggle eshell"
-  (interactive)
-  (if (equal major-mode 'eshell-mode)
-      (switch-to-prev-buffer)
-    (aweshell-next)))
-
-;; https://github.com/manuel-uberti/.emacs.d/commit/d44051ef417aee2086ba05e5a514e0ce6c401ca7
-(defun counsel-eshell ()
-  "Browse Eshell history."
-  (interactive)
-  (setq ivy-completion-beg (point))
-  (setq ivy-completion-end (point))
-  (ivy-read "Symbol name: "
-            (delete-dups
-             (ring-elements eshell-history-ring))
-            :action #'ivy-completion-in-region-action))
-
-
 (use-package| (aweshell :fetcher github :repo "manateelazycat/aweshell")
   :commands (aweshell-new aweshell-next aweshell-prev aweshell-toggle)
   :init (setq aweshell-use-exec-path-from-shell nil)
@@ -62,45 +43,11 @@
 (setq eshell-directory-name (concat moon-star-dir "utility/eshell/"))
 (setq eshell-last-dir-ring-size 500)
 (setq eshell-cp-interactive-query t
-       eshell-ln-interactive-query t
-       eshell-mv-interactive-query t
-       eshell-rm-interactive-query t
-       eshell-mv-overwrite-files nil)
+      eshell-ln-interactive-query t
+      eshell-mv-interactive-query t
+      eshell-rm-interactive-query t
+      eshell-mv-overwrite-files nil)
 
 ;; /ssh:/10.52.224.67:blah
 ;; to: /10.52.224.67:blah
 (setq tramp-default-method "ssh")
-
-
-;;;; History
-
-;; https://emacs-china.org/t/topic/4579?u=samray
-(defun moon/esh-history ()
-  "Interactive search eshell history."
-  (interactive)
-  (require 'em-hist)
-  (save-excursion
-    (let* ((start-pos (eshell-bol))
-	   (end-pos (point-at-eol))
-	   (input (buffer-substring-no-properties start-pos end-pos)))
-      (let* ((command (ivy-read "Command: "
-				(delete-dups
-				 (when (> (ring-size eshell-history-ring) 0)
-				   (ring-elements eshell-history-ring)))
-				:preselect input
-				:action #'ivy-completion-in-region-action))
-	     (cursor-move (length command)))
-	(kill-region (+ start-pos cursor-move) (+ end-pos cursor-move)))))
-  (end-of-line))
-
-
-;;;; Swtich eshell
-
-(defun counsel-switch-to-eshell-buffer ()
-    "Switch to a shell buffer, or create one."
-    (interactive)
-    (require 'counsel)
-    (ivy-read "Eshell buffer: " (counsel--buffers-with-mode #'eshell-mode)
-              :action #'counsel--switch-to-shell
-              :caller 'counsel-switch-to-shell-buffer))
-
