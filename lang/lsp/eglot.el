54e7b44363223c38a18bcb2971e1d57b0b970850
Yuan Fu <casouri@gmail.com>
2018-12-25 02:26:49 -0500

    Add my hel-at-point hack and disable eldoc-box-mode by default

--- lang/lsp/eglot.el
+++ lang/lsp/eglot.el
@@ -14,7 +14,9 @@
     "l f" #'eglot-format-buffer
     "l R" #'eglot-rename
     "l d" #'xref-find-definitions
-    "l r" #'xref-find-references))
+    "l r" #'xref-find-references)
+  (general-define-key
+   "C-h C-h" #'moon-help-at-point))
 
 ;;; Packages
 
@@ -30,7 +32,22 @@
   :commands (eldoc-box-hover-mode
              eldoc-box-help-at-point)
   :init
-  (add-hook 'eglot--managed-mode-hook #'eldoc-box-hover-mode t)
+  ;; (add-hook 'eglot--managed-mode-hook #'eldoc-box-hover-mode t)
   (setq eldoc-box-cleanup-interval 0.2))
+
+;;; Function
+
+(defun moon-help-at-point ()
+  (interactive)
+  (when eglot--managed-mode
+    (require 'eldoc-box)
+    (let ((eldoc-box-position-function #'eldoc-box--default-at-point-position-function))
+      (eldoc-box--display
+       (eglot--dbind ((Hover) contents range)
+           (jsonrpc-request (eglot--current-server-or-lose) :textDocument/hover
+                            (eglot--TextDocumentPositionParams))
+         (when (seq-empty-p contents) (eglot--error "No hover info here"))
+         (eglot--hover-info contents range))))
+    (add-hook 'pre-command-hook #'eldoc-box-quit-frame t t)))
 
 ;;; eglot.el ends here
