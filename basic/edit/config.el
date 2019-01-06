31555b378ebdc376700a754730d763a7caf72f28
Yuan Fu <casouri@gmail.com>
2019-01-06 13:53:46 -0500

    Add back/forward buttons for helpful

--- basic/edit/config.el
+++ basic/edit/config.el
@@ -172,7 +172,50 @@
                           ;; ensure the helpful window is selected for `helpful-update'.
                           (select-window window)
                         ;; line above returns nil if no available window is found
-                        (pop-to-buffer buf)))))
+                        (pop-to-buffer buf))))
+  (defvar moon-helpful-history () "History of helpful, a list of buffers.")
+  (advice-add #'helpful-update :around #'moon-helpful@helpful-update)
+  (advice-add #'helpful--buffer :around (lambda (oldfunc &rest _)
+                                          (let ((buf (apply oldfunc _)))
+                                            (push buf moon-helpful-history)
+                                            buf))))
+
+(defun moon-helpful@helpful-update (oldfunc)
+  "Insert back/forward buttons."
+  (funcall oldfunc)
+  (let ((inhibit-read-only t))
+    (goto-char (point-min))
+    (insert-text-button "Back"
+                        'action (lambda (&rest _)
+                                  (interactive)
+                                  (moon-helpful-switch-to-buffer (current-buffer) 1)))
+    (insert " / ")
+    (insert-text-button "Forward"
+                        'action (lambda (&rest _)
+                                  (interactive)
+                                  (moon-helpful-switch-to-buffer (current-buffer)  -1)))
+    (insert "\n\n")))
+
+(defun moon-helpful-switch-to-buffer (buffer &optional offset)
+  "Jump to last SYMBOL in helpful history, offset by OFFSET."
+  (interactive)
+  (require 'seq)
+  (require 'cl-lib)
+  (setq moon-helpful-history (seq-remove (lambda (buf) (not (buffer-live-p buf))) moon-helpful-history))
+  (cl-labels ((find-index (elt lst)
+                          (let ((idx 0)
+                                (len (length lst)))
+                            (while (and (not (eq elt (nth idx lst)))
+                                        (not (eq idx len)))
+                              (setq idx (1+ idx)))
+                            (if (eq idx len)
+                                nil
+                              idx))))
+    (let ((idx (+ (or offset 0) (find-index buffer moon-helpful-history))))
+      (if (or (>= idx (length moon-helpful-history))
+              (< idx 0))
+          (message "No further history.")
+        (switch-to-buffer (nth idx moon-helpful-history))))))
 
 ;;; Config
 
