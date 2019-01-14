cfd405362e9e5f69671c403facb8bdf4cb41e07a
Yuan Fu <casouri@gmail.com>
2019-01-14 10:42:29 -0500

    Add pdf star

---
+++ utility/pdf/config.el
@@ -0,0 +1,15 @@
+;;; config.el --- PDF viewing      -*- lexical-binding: t; -*-
+
+;; Author: Yuan Fu <casouri@gmail.com>
+
+;;; This file is NOT part of GNU Emacs
+
+;;; Commentary:
+;;
+
+;;; Code:
+;;
+
+(setq doc-view-resolution 320)
+
+;;; config.el ends here
