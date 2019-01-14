256224ddc39a54c1699d12603439ac3fab1a3e05
Yuan Fu <casouri@gmail.com>
2019-01-14 10:41:05 -0500

    Add java config

--- lang/java/config.el
+++ lang/java/config.el
@@ -16,35 +16,13 @@
 ;;
 ;;;;; LSP
 
-;; don't use lsp yet
-;;
-;; (moon-lsp/eglot
-;;  (progn
-;;    (add-hook 'java-mode-hook #'lsp t)
-;;    (push '(java-mode . lsp-format-buffer) moon-smart-format-alist))
-;;  (progn
-;;    (add-hook 'java-mode-hook #'eglot-ensure)
-;;    (push '(java-mode . eglot-format-buffer) moon-smart-format-alist)))
-
-;; https://github.com/mopemope/meghanada-emacs
-;;
-
-(use-package| meghanada
-  :config
-  (add-hook 'java-mode-hook
-            (lambda ()
-              (meghanada-mode)
-              (flycheck-mode)
-              ))
-  (add-to-list 'moon-smart-format-alist '(java-mode . meghanada-code-beautify-before-save))
-  (cond
-   ((eq system-type 'windows-nt)
-    (setq meghanada-java-path (expand-file-name "bin/java.exe" (getenv "JAVA_HOME")))
-    (setq meghanada-maven-path "mvn.cmd"))
-   (t
-    (setq meghanada-java-path "java")
-    (setq meghanada-maven-path "mvn")))
-  )
+(moon-lsp/eglot
+ (progn
+   (add-hook 'java-mode-hook #'lsp t)
+   (push '(java-mode . lsp-format-buffer) moon-smart-format-alist))
+ (progn
+   (add-hook 'java-mode-hook #'eglot-ensure)
+   (push '(java-mode . eglot-format-buffer) moon-smart-format-alist)))
 
 
 
