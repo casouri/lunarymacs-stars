ffbf9e9159acd7a59c9aacf11c523b58be81a9f1
Yuan Fu <casouri@gmail.com>
2019-01-14 10:41:35 -0500

    Clean up eshell config
    I revised aweshell

--- utility/eshell/config.el
+++ utility/eshell/config.el
@@ -11,29 +11,16 @@
     "yp" #'aweshell-prev
     "yN" #'aweshell-new)
   (general-define-key
-   "s-e" #'aweshell-toggle))
+   "s-e" #'aweshell-toggle)
+  (general-define-key
+   :keymaps 'aweshell-mode-map
+   "<tab>" #'company-complete))
 
 (use-package| aweshell
   :commands (aweshell-new aweshell-next aweshell-prev aweshell-toggle)
-  :init
-  (setq aweshell-use-exec-path-from-shell nil)
-  (add-hook
-   'eshell-mode-hook
-   (lambda ()
-     (company-mode)
-     (setq-local company-auto-complete nil)
-     (setq-local company-idle-delay 9999)
-     (esh-autosuggest-companyless-mode)
-     (mve
-      (with-eval-after-load 'general
-        (with-eval-after-load 'evil
-          (general-define-key
-           :states 'insert
-           :keymaps 'eshell-mode-map
-           "C-p" #'eshell-previous-matching-input-from-input
-           "C-n" #'eshell-next-matching-input-from-input)))
-      (define-key eshell-mode-map
-        (kbd "<tab>") #'company-complete)))))
+  :config
+  (setq eshell-highlight-prompt nil
+        eshell-prompt-function 'epe-theme-pipeline))
 
 ;;; Config
 
