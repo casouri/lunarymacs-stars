110f5ef18ad6b4fff6cc60fa499bd224eb0728cf
Yuan Fu <casouri@gmail.com>
2019-01-24 16:04:33 -0500

    Add text config

--- utility/tex/config.el
+++ utility/tex/config.el
@@ -1,16 +1,51 @@
-;; suggested by auctex manual
-(setq TeX-auto-save t)
-(setq TeX-parse-self t)
-(setq TeX-syntactic-comment t)
-(setq-default TeX-master nil)
+;;; config.el --- Tex      -*- lexical-binding: t; -*-
 
-(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
-(add-hook 'LaTeX-mode-hook 'TeX-PDF-mode)
+;; Author: Yuan Fu <casouri@gmail.com>
 
-(use-package| latex-preview-pane
-  :commands latex-preview-pane-mode)
+;;; This file is NOT part of GNU Emacs
+
+;;; Commentary:
+;;
+
+;;; Code:
+;;
+;;;; Key
 
 (post-config| general
   (moon-default-leader
     :keymaps 'tex-mode-map
     "tp" #'latex-preview-pane-mode))
+
+;;;; Packages
+
+(use-package| latex-preview-pane
+  :commands latex-preview-pane-mode)
+
+(use-package| company-math
+  :after company)
+
+(defun moon-latex-company-setup ()
+  (setq-local company-backends
+              (append '((company-math-symbols-latex company-latex-commands))
+                      company-backends)))
+
+(add-hook 'tex-mode-hook 'my-latex-mode-setup)
+
+;; (post-config| eglot
+;;   (add-to-list 'eglot-server-programs '(latex-mode . ("digestif"))))
+
+;;;; Function
+;;
+;; auto {} after \
+
+(defun moon-tex-post-insert (&rest _)
+  "Insert {} after \\."
+  (interactive)
+  (when (eq (char-before) ?\\)
+    (insert "{}")
+    (backward-char 2)))
+
+(add-hook 'latex-mode-hook (lambda ()
+                             (add-hook 'post-self-insert-hook #'moon-tex-post-insert nil t)))
+
+;;; config.el ends here
