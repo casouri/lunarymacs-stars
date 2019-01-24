2e49a38981236c158005e9f9887185f6f3afa269
Yuan Fu <casouri@gmail.com>
2019-01-24 16:04:50 -0500

    Add assembly config

---
+++ lang/assembly/config.el
@@ -0,0 +1,17 @@
+;;; config.el --- Assembly      -*- lexical-binding: t; -*-
+
+;; Author: Yuan Fu <casouri@gmail.com>
+
+;;; This file is NOT part of GNU Emacs
+
+;;; Commentary:
+;; 
+
+;;; Code:
+;;
+
+(use-package| mips-mode
+  :mode "\\.mips$")
+
+
+;;; config.el ends here
