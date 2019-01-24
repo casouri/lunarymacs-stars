d239c0610a4ad9036cf96cef2990cdb4f3fa5cc9
Yuan Fu <casouri@gmail.com>
2019-01-24 16:04:59 -0500

    Change mac config
    Remove everything
    
    exec-path is set already because my Emacs.app is just a shell script

--- os/mac/config.el
+++ os/mac/config.el
@@ -6,16 +6,6 @@
 ;;; Code:
 ;;
 
-(defun get-path ()
-  (let ((path-raw (shell-command-to-string "/usr/libexec/path_helper")))
-    (string-match "PATH=\"\\(.+?\\)\"" path-raw)
-    (match-string 1 path-raw)))
-
-(let ((path (get-path)))
-  (setenv "PATH" path)
-  (setq exec-path (append exec-path (split-string path ":"))))
-
-
-(load| path)
+;; (setq exec-path (append exec-path (split-string (getenv "PATH") ":")))
 
 ;;; config.el ends here
