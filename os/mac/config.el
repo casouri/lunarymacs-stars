ae876a3bac6cb1ee43cf928db00b8035d212a4bd
Yuan Fu <casouri@gmail.com>
2019-01-28 10:53:56 -0500

    Translate mouse-3 to mouse-2 on mac

--- os/mac/config.el
+++ os/mac/config.el
@@ -1,4 +1,4 @@
-;;; config.el --- PATH and friends      -*- lexical-binding: t; -*-
+;;; config.el --- Config specific to Mac      -*- lexical-binding: t; -*-
 
 ;;; Commentary:
 ;; 
@@ -7,5 +7,9 @@
 ;;
 
 ;; (setq exec-path (append exec-path (split-string (getenv "PATH") ":")))
+
+;; on mac, double click touch pad is considered mouse-3,
+;; but most functions bound to mouse-2
+(define-key input-decode-map [mouse-3] [mouse-2])
 
 ;;; config.el ends here
