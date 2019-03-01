8ad827a812a290c8cc8b09f4ce54f36011e941c4
Yuan Fu <casouri@gmail.com>
2019-03-01 18:41:52 -0500

    Remove eldoc-box-helpful stuff

--- lang/lsp/eglot.el
+++ lang/lsp/eglot.el
@@ -42,50 +42,6 @@
 
 ;;;; ElDoc-Box
 
-;;;;; Helpful
-
-(defun eldoc-box-helpful-variable (symbol)
-  "Show help for variable named SYMBOL."
-  (interactive
-   (list (helpful--read-symbol "Variable: " #'helpful--variable-p)))
-  (let ((buffer (helpful--buffer symbol nil)))
-    (with-current-buffer buffer
-      (helpful-update)
-      (setq mode-line-format nil))
-    (eldoc-box--get-frame buffer)))
-
-(defun eldoc-box-helpful-callable (symbol)
-  "Show help for function, macro or special form named SYMBOL.
-
-See also `helpful-macro' and `helpful-function'."
-  (interactive
-   (list (helpful--read-symbol "Callable: " #'fboundp)))
-  (let ((buffer (helpful--buffer symbol t)))
-    (with-current-buffer buffer
-      (helpful-update)
-      (setq mode-line-format nil))
-    (eldoc-box--get-frame buffer)))
-
-(defun eldoc-box-helpful-key (key-sequence)
-  "Show help for interactive command bound to KEY-SEQUENCE."
-  (interactive
-   (list (read-key-sequence "Press key: ")))
-  (let ((sym (key-binding key-sequence)))
-    (cond
-     ((null sym)
-      (user-error "No command is bound to %s"
-                  (key-description key-sequence)))
-     ((commandp sym)
-      (let ((buffer (helpful--buffer sym t)))
-        (with-current-buffer buffer
-          (helpful-update)
-          (setq mode-line-format nil))
-        (eldoc-box--get-frame buffer)))
-     (t
-      (user-error "%s is bound to %s which is not a command"
-                  (key-description key-sequence)
-                  sym)))))
-
 (defun eldoc-box-show-frame ()
   "Show childframe."
   (interactive)
