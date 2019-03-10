0f69f27e3e1a5477430470d0dba8dcc8c3f4c1d3
Yuan Fu <casouri@gmail.com>
2019-03-10 17:23:55 -0400

    Disable vterm

--- utility/shell/config.el
+++ utility/shell/config.el
@@ -28,32 +28,32 @@
              fshell
              fshell-toggle))
 
-(use-package| vterm
-  :init
-  (add-hook 'load-path "~/attic/emacs-libvterm")
-  (setq vterm-shell "zsh")
-  :commands vterm
-  :config
-  (define-key vterm-mode-map (kbd "C-p") nil)
-  (define-key vterm-mode-map (kbd "C-n") nil)
-  (define-key vterm-mode-map (kbd "C-e") nil)
-  (define-key vterm-mode-map (kbd "<backspace>") #'vterm--self-insert))
+;; (use-package| vterm
+;;   :init
+;;   (add-hook 'load-path "~/attic/emacs-libvterm")
+;;   (setq vterm-shell "zsh")
+;;   :commands vterm
+;;   :config
+;;   (define-key vterm-mode-map (kbd "C-p") nil)
+;;   (define-key vterm-mode-map (kbd "C-n") nil)
+;;   (define-key vterm-mode-map (kbd "C-e") nil)
+;;   (define-key vterm-mode-map (kbd "<backspace>") #'vterm--self-insert))
 
 
-(defun toggle-vterm ()
-  (interactive)
-  (if (eql major-mode 'vterm-mode)
-      (while (eql major-mode 'vterm-mode)
-        (switch-to-prev-buffer))
-    (let ((buf-lst (buffer-list))
-          found)
-      (if (dolist (buf buf-lst found)
-            (when (eql (buffer-local-value 'major-mode buf)
-                       'vterm-mode)
-              (switch-to-buffer buf)
-              (setq found t)))
-          nil
-        (vterm)))))
+;; (defun toggle-vterm ()
+;;   (interactive)
+;;   (if (eql major-mode 'vterm-mode)
+;;       (while (eql major-mode 'vterm-mode)
+;;         (switch-to-prev-buffer))
+;;     (let ((buf-lst (buffer-list))
+;;           found)
+;;       (if (dolist (buf buf-lst found)
+;;             (when (eql (buffer-local-value 'major-mode buf)
+;;                        'vterm-mode)
+;;               (switch-to-buffer buf)
+;;               (setq found t)))
+;;           nil
+;;         (vterm)))))
 
 
 ;;; config.el ends here
